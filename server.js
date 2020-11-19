b4c0c097c5dbd8a88862ffc73eaf8db5ff382e95
Your Name <you@example.com>
2020-11-19 21:55:07 +0100

    set CSP Header

--- server.js
+++ server.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const app = express();
 let port = process.env.PORT || 8080;
-app.use(express.static('./dist/cool-page-ui'));
+
+const connectSrc = defineConnectSrc();
+
+var options = {
+  setHeaders: function (res, path, stat) {
+    res.set("Content-Security-Policy", 
+    "default-src 'self'; " +
+    "script-src 'self' https://apis.google.com; "+
+    "style-src 'self' 'unsafe-inline'; "+
+    "frame-src 'self' https://accounts.google.com/; " + 
+    "connect-src 'self' " + connectSrc)
+  }
+}
+
+app.use(express.static('./dist/cool-page-ui', options));
+console.log('connect src url set to : ' , connectSrc);
 
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: './dist/cool-page-ui/'});
-  console.log(' request received : ' , req)
 });
+
 app.listen(port, () => {
-                                       console.log(`Example app is listening on port http://localhost:${port}`);
-                                   }
+    console.log(`Example app is listening on port http://localhost:${port}`);
+});
 
-);
-
+/*
+TODO Kopple die Backend Url ans environment von angular
+ */
+function defineConnectSrc() {
+  if(process.env.IS_LOCAL) {
+    return 'http://localhost:5000';
+  }
+  return 'https://cool-page.herokuapp.com';
+}
